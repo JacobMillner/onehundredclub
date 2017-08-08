be75d3aa97e983b5cde6ef6ed59f61db0c29c972
Jacob Millner <jacob.millner@outlook.com>
2017-08-08 14:00:43 +0000

    restricted posting to once a day

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,19 @@ class PostsController < ApplicationController
   before_action :authenticate_user! , only: [:create, :destroy]
 
   def create
-    @post = current_user.posts.build
-    if @post.save
-      flash[:success] = "Post Successful!"
+    last_post = current_user.posts.last
+    #make sure they haven't posted already today
+    if last_post != nil and last_post.created_at.to_date == Date.current
+      flash[:failure] = "You cannot post more than once a day!"
       redirect_to root_url
     else
-      render 'root'
+      @post = current_user.posts.build
+      if @post.save
+        flash[:success] = "Post Successful!"
+        redirect_to root_url
+      else
+        render 'root'
+      end
     end
   end
 
